07122ece73ca29883d283a40e4f96422a7e825d3
Клишенков Артём Игоревич БСБО-06-23 <73350652+artemKlishenkov@users.noreply.github.com>
2025-05-02 19:00:35 +0300

    Update ExampleInstrumentedTest.java

--- number_five/src/androidTest/java/ru/mirea/klishenkovai/number_five/ExampleInstrumentedTest.java
+++ number_five/src/androidTest/java/ru/mirea/klishenkovai/number_five/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package ru.mirea.klishenkovaai.number_five;
+package ru.mirea.klishenkovai.number_five;
 
 import android.content.Context;
 
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("ru.mirea.klishenkovaai.number_five", appContext.getPackageName());
+        assertEquals("ru.mirea.klishenkovai.number_five", appContext.getPackageName());
     }
 }
