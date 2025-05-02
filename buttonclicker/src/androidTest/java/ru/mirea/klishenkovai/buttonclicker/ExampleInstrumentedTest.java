16f404cb9c5dc3fd596b8f461c2cd86d71c8acca
Клишенков Артём Игоревич БСБО-06-23 <73350652+artemKlishenkov@users.noreply.github.com>
2025-05-02 18:11:51 +0300

    Update ExampleInstrumentedTest.java

--- buttonclicker/src/androidTest/java/ru/mirea/klishenkovai/buttonclicker/ExampleInstrumentedTest.java
+++ buttonclicker/src/androidTest/java/ru/mirea/klishenkovai/buttonclicker/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package ru.mirea.klishenkovaai.buttonclicker;
+package ru.mirea.klishenkovai.buttonclicker;
 
 import android.content.Context;
 
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("ru.mirea.klishenkovaai.buttonclicker", appContext.getPackageName());
+        assertEquals("ru.mirea.klishenkovai.buttonclicker", appContext.getPackageName());
     }
 }
