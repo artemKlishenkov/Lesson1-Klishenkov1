50cc73a9896f82f54daa40f6305e5c8db4affdfb
Клишенков Артём Игоревич БСБО-06-23 <73350652+artemKlishenkov@users.noreply.github.com>
2025-05-02 17:52:10 +0300

    Update ExampleInstrumentedTest.java

--- app/src/androidTest/java/ru/mirea/KlishenkovAI/lesson1/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/ru/mirea/KlishenkovAI/lesson1/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package ru.mirea.KlishenkovaAI.lesson1;
+package ru.mirea.KlishenkovAI.lesson1;
 
 import android.content.Context;
 
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("ru.mirea.KlishenkovaAI.lesson1", appContext.getPackageName());
+        assertEquals("ru.mirea.KlishenkovAI.lesson1", appContext.getPackageName());
     }
 }
