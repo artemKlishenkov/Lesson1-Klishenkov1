5bb53747ca6ca4e01340709f1940b8b72d471ddd
Клишенков Артём Игоревич БСБО-06-23 <73350652+artemKlishenkov@users.noreply.github.com>
2025-05-02 18:08:29 +0300

    Update build.gradle.kts

--- buttonclicker/build.gradle.kts
+++ buttonclicker/build.gradle.kts
@@ -3,11 +3,11 @@ plugins {
 }
 
 android {
-    namespace = "ru.mirea.klishenkovaai.buttonclicker"
+    namespace = "ru.mirea.klishenkovai.buttonclicker"
     compileSdk = 35
 
     defaultConfig {
-        applicationId = "ru.mirea.klishenkovaai.buttonclicker"
+        applicationId = "ru.mirea.klishenkovai.buttonclicker"
         minSdk = 26
         targetSdk = 35
         versionCode = 1
