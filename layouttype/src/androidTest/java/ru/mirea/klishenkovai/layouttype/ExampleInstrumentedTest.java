fc2a7af5e4ff09761bcc854ffeb9298129788bb1
Клишенков Артём Игоревич БСБО-06-23 <73350652+artemKlishenkov@users.noreply.github.com>
2025-05-02 18:36:36 +0300

    Update ExampleInstrumentedTest.java

--- layouttype/src/androidTest/java/ru/mirea/klishenkovai/layouttype/ExampleInstrumentedTest.java
+++ layouttype/src/androidTest/java/ru/mirea/klishenkovai/layouttype/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package ru.mirea.klishenkovaai.layouttype;
+package ru.mirea.klishenkovai.layouttype;
 
 import android.content.Context;
 
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("ru.mirea.klishenkovaai.layouttype", appContext.getPackageName());
+        assertEquals("ru.mirea.klishenkovai.layouttype", appContext.getPackageName());
     }
 }
